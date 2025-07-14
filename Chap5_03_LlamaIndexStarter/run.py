e00c4b54214c3bbac9c89d1939705beb0645f6bb
Jeff Xia <yang.xia@china.talkdesk.com>
2025-07-14 23:13:42 +0800

    commit

--- Chap5_03_LlamaIndexStarter/run.py
+++ Chap5_03_LlamaIndexStarter/run.py
@@ -1,11 +1,10 @@
-
 from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 load_dotenv()
 
 # Load and index the documents in the data folder
-documents = SimpleDirectoryReader("files").load_data()
+documents = SimpleDirectoryReader("../files").load_data()
 index = VectorStoreIndex.from_documents(documents)
 
 # Query your data
